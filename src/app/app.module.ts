4592e3ef926f975822a5756456b5d94db973b5c5
Roicel Laya <roicel.laya@gmail.com>
2022-08-16 21:48:19 -0400

    Updated app module file.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RoadmapComponent } from './roadmap/roadmap.component';
+import { RoadmapHeaderComponent } from './roadmap-header/roadmap-header.component';
+import { RoadmapBodyComponent } from './roadmap-body/roadmap-body.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskComponent } from './task/task.component';
+import { BartimeDirective } from './directives/bartime.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoadmapComponent,
+    RoadmapHeaderComponent,
+    RoadmapBodyComponent,
+    TaskListComponent,
+    TaskComponent,
+    BartimeDirective
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: []
 })
 export class AppModule { }
