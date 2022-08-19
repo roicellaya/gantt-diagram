b2804f7da4a0f089c8087ebf2baf3c0a1e6070e8
Roicel Laya <roicel.laya@gmail.com>
2022-08-19 19:44:36 -0400

    Continuing restructuring.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BartimeComponent } from './components';
-import { BarCurrentDateComponent } from '../bar-current-date/bar-current-date.component';
 
 @NgModule({
   declarations: [
-    BartimeComponent,
-    BarCurrentDateComponent
+    BartimeComponent
   ],
   imports: [
     CommonModule
